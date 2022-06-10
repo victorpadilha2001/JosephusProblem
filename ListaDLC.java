4f2f12eb7fb788999cd273d265fb23b3d96ea867
victor padilha <82392118+victorpadilha2001@users.noreply.github.com>
2022-06-10 08:00:56 -0300

    Add files via upload

---
+++ ListaDLC.java
@@ -0,0 +1,16 @@
+import java.util.random.*;
+
+
+public class ListaDLC {
+	private No inicio;
+	private No fim;
+	
+	int tamanho;
+	private Random r = new Random(System.currentTimeMillis());
+	
+	
+	public ListaDLC() {
+		setInicio(null);
+	}
+	
+}
